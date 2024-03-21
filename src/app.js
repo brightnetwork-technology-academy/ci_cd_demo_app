befd95f73d1120aef2af9da6acf56ace342d18ec
Zsolt Podoba-Szalai <podobaszalaizsolt@gmail.com>
2024-03-21 16:45:50 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const teas = [
+    {name: "Rooibos", brand: "Twinnings"},
+    {name: "Earl Grey", brand: "Tetley"},
+    {name: "Breakfast", brand: "Yorkshire Tea"},
+    {name: "Green", brand: "Lipton"},
+    {name: "Yerba Mate", brand: "Some argentinian brand, idk"},
+    {name: "white tea", brand: "lol"}
+]
+
+app.get("/teas", (req, res) => {
+    res.status(200).json(teas)
+})
+
+app.listen(port, () => {
+    console.log(`Demo app listening on port ${port}!`)
+})
+
+module.exports = app;
---
+++ src/app.test.js
@@ -0,0 +1,11 @@
+const app = require("./app")
+const supertest = require("supertest")
+const request = supertest(app)
+
+describe("/teas endpoint", () => {
+    it("should return a response", async () => {
+        const response = await request.get("/teas")
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(4);
+    })
+})
