bf4b97949e1d7e41f8d03ad56f79db8bee6e319c
Anna Henderson <annahenderson@gmail.com>
2024-06-27 23:17:05 +0100

    adds tea and alters test

--- src/app.test.js
+++ src/app.test.js
@@ -6,6 +6,6 @@ describe("/teas endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/teas")
         expect(response.status).toBe(200);
-        expect(response.body.length).toBe(4);
+        expect(response.body.length).toBe(5);
     })
 })
